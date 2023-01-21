16d7c19a1ae58e2440841e62eba55136d3e09620
Branden van Staden <92578986+BrandenSysoutHelloWorld@users.noreply.github.com>
2023-01-21 17:43:58 +0200

    Add project files.

---
+++ WebScrapper_Prototype/Areas/Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebScrapper_Prototype.Areas.Identity.Data;
+
+namespace WebScrapper_Prototype.Areas.Identity.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new ApplicationUserEntityConfigure());
+    }
+}
+
+public class ApplicationUserEntityConfigure : IEntityTypeConfiguration<ApplicationUser>
+{
+    void IEntityTypeConfiguration<ApplicationUser>.Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.Property(u => u.FirstName).IsRequired();
+        builder.Property(u => u.LastName).IsRequired();
+        builder.Property(u => u.CountryCode).IsRequired();
+        builder.Property(u => u.Cellphone).IsRequired();
+
+        builder.Property(u => u.CountryCode).HasMaxLength(25);
+        builder.Property(u => u.FirstName).HasMaxLength(255);
+        builder.Property(u => u.LastName).HasMaxLength(255);
+
+
+    }
+}
