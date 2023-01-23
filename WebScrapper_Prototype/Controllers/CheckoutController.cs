b7a6ea2b6b2900f2444153f19b3b07261f50a661
Branden van Staden <92578986+BrandenSysoutHelloWorld@users.noreply.github.com>
2023-01-23 18:12:43 +0200

    Version0.1.5 Added: Checkout and Product webpage

---
+++ WebScrapper_Prototype/Controllers/CheckoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebScrapper_Prototype.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebScrapper_Prototype/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebScrapper_Prototype.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
